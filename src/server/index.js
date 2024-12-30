6799c0038903451be74c69fd72a297075b002591
Andrew Scoppa <andrew-scoppa@github.com>
2024-12-30 18:20:13 -0500

    xhanged comments in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
+/*
+
+This file is the entry point for the server. It creates an Express server and serves the static files from the public directory. It also serves the index.html file for all the routes.
+
+*/
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, '../../public')));
 
+// Handle any requests that don't match the ones above
 app.get('*', (req, res) => {
   console.log(`Request received for: ${req.url}`);
   const filePath = path.join(__dirname, '../../public/index.html');
@@ -12,6 +20,7 @@ app.get('*', (req, res) => {
   res.sendFile(filePath);
 });
 
+// Start the server on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
